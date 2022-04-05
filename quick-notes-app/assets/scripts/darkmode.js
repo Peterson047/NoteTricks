07550e3adbffcf738b1bf4c31ef6ba4de76cf4d7
Peterson Alves <peterrson047@gmail.com>
2022-04-04 21:15:19 -0300

    inicio correção modo escuro

--- quick-notes-app/assets/scripts/darkmode.js
+++ quick-notes-app/assets/scripts/darkmode.js
@@ -1,9 +1,12 @@
 // Select the button
 const btn = document.querySelector('.btn-toggle');
+const nav = document.getElementById('myDIV')
+
 
 
 // Listen for a click on the button
 btn.addEventListener('click', function () {
     // Then toggle (add/remove) the .dark-theme class to the body
-    document.body.classList.toggle('dark-theme'), ('tab-color');
+    document.body.classList.toggle('dark-theme'), ('tab-color'), ('tabs');
+    nav.classList.toggle('tab-color')
 })
